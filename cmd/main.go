97fa44cc2676181127ba41dc881f61fbb21b3009
alex-salamatov <alex.salamatov@gmail.com>
2026-02-11 09:42:42 +0100

    added simplest http server

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/alex-salamatov/http_server_hw/pkg/httpserver"
 )
 
 func main() {
 	fmt.Println("HTTP server Hello World")
+
+	httpserver.RunHttpSrv()
 }
