eb79eb36f8ed456c64947f39c8937ca819476f95
alex-salamatov <alex.salamatov@gmail.com>
2026-02-13 15:14:07 +0100

    added handler of /download path

--- pkg/httpserver/httpsrv.go
+++ pkg/httpserver/httpsrv.go
@@ -5,11 +5,25 @@ import (
 	"net/http"
 )
 
+func HandleDownload(w http.ResponseWriter, r *http.Request) {
+
+	download_folder := r.URL.Query().Get("id")
+	if download_folder == "" {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprint(w, "Download files from ")
+	fmt.Fprintln(w, download_folder)
+}
+
 func RunHttpSrv() {
 	http.HandleFunc("/hw", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Hello World")
 	})
 
-	fmt.Println("Server started on http://localhost:8080")
+	http.HandleFunc("/download/", HandleDownload)
+
+	fmt.Println("SServer started on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
