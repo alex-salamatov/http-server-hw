1f50059fb4a878273d182cc15c23da9a51e78ff5
alex-salamatov <alex.salamatov@gmail.com>
2026-02-15 22:06:06 +0100

    added simple version for files downloader for directory specified in command line argument

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/alex-salamatov/http_server_hw/pkg/httpserver"
 )
 
 func main() {
-	fmt.Println("HTTP server Hello World")
+	fmt.Println("HTTP server Hello World with files download")
 
-	httpserver.RunHttpSrv()
+	// Command‑line flags
+	dir := flag.String("dir", ".", "directory for downloading")
+	flag.Parse()
+
+	httpserver.RunHttpSrv(*dir)
 }
